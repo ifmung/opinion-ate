69ced719dc102b22db96eb210b3792008bb5c92f
if <if.mung@gmail.com>
2024-08-20 17:02:54 +0800

    Add RestaurantScreen and RestaurantList

---
+++ src/components/RestaurantScreen.js
@@ -0,0 +1,10 @@
+import { RestaurantList } from "./RestaurantList";
+
+export function RestaurantScreen() {
+  return (
+    <div>
+      <h1>Restaurants</h1>
+      <RestaurantList />
+    </div>
+  );
+}
