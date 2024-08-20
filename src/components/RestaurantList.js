eeb8d085e84082462a924b243788b3da4d7ff5d5
if <if.mung@gmail.com>
2024-08-20 18:02:02 +0800

    Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,3 +1,8 @@
-export function RestaurantList() {
+import { useEffect } from "react";
+
+export function RestaurantList({ loadRestaurants }) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
   return "list";
 }
